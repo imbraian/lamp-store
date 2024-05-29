b789e849a65d184b5ea654c68a19ff8dd273993d
imbraian <157419801+imbraian@users.noreply.github.com>
2024-05-29 00:26:53 -0300

    upload home section

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,12 @@ navLink.forEach(link => {
 });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50 ? header.classList.add("bg-header") : header.classList.remove("bg-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
 
 /*=============== SWIPER POPULAR ===============*/
 
