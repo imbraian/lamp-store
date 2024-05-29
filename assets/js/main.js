f3f8efd5bdeb126443f556fbc4b54c398f72a92e
imbraian <157419801+imbraian@users.noreply.github.com>
2024-05-29 16:46:26 -0300

    upload products section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
-/*=============== MENU ===============*/
-const navMenu = document.getElementById("nav-menu");
-const navToggle = document.getElementById("nav-toggle");
-const navClose = document.getElementById("nav-close");
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 if (navToggle) {
   navToggle.addEventListener("click", () => {
@@ -19,21 +18,34 @@ const navLink = document.querySelectorAll(".nav__link");
 const linkAction = () => {
   const navMenu = document.getElementById("nav-menu");
   navMenu.classList.remove("show-menu");
-}
+};
 
-navLink.forEach(link => {
+navLink.forEach((link) => {
   link.addEventListener("click", linkAction);
 });
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
 const scrollHeader = () => {
   const header = document.getElementById("header");
-  this.scrollY >= 50 ? header.classList.add("bg-header") : header.classList.remove("bg-header");
-}
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
 window.addEventListener("scroll", scrollHeader);
 
-
-/*=============== SWIPER POPULAR ===============*/
+const popularSwiper = new Swiper(".popular__content", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      centeredSlides: false,
+    },
+  },
+});
 
 /*=============== CHOOSE FAQ ===============*/
 
